2d9027331b4a8a07dbe536d9edbcecc9cd5ac49d
Abdelrahman Nasr <123078186+Nasr275@users.noreply.github.com>
2024-05-26 19:44:10 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "SDB.h"
+#include "STD.h"
+
+
+/**Our main function that only calls SDB_APP function which in turn calls SDB_action*/
+int main()
+{
+    SDB_APP();
+    return 0;
+}
